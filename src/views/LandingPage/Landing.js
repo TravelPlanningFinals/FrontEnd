bd7610a9500876068782b90a3037e0f975f16354
Chase Crowder <gcrowder93@gmail.com>
2022-04-12 17:27:06 -0700

    calendar is commented out, and date selections work for start and end

--- src/views/LandingPage/Landing.js
+++ src/views/LandingPage/Landing.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './Landing.css';
 
 export default function Landing() {
+  const [date, setDate] = useState('');
+  const [returnDate, endDate] = useState('');
+
   return (
     <>
       <h1>start your next adventure</h1>
       <input placeholder="location"></input>
-      <input placeholder="dates"></input>
+      <br></br>
+      {/* <input placeholder="dates"></input> */}
+      <br></br>
+      <mark>Trip Starting Date:</mark>
+      <input
+        className="controls"
+        type="date"
+        placeholder="Starting Date"
+        value={date}
+        onChange={(e) => {
+          setDate(e.target.value);
+        }}
+      />
+      <br></br>
+      <mark className="end">Trip Ending Date:</mark>
+
+      <input
+        className="controls"
+        type="date"
+        placeholder="Ending Date"
+        value={returnDate}
+        onChange={(e) => {
+          endDate(e.target.value);
+        }}
+      />
       <br></br>
       <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
         Plan your trip
