ac97558f0f2f2ae3c1c603867603b4e63de0f794
Brett Seifried <seifriedbrett@gmail.com>
2022-04-13 17:12:12 -0700

    fixed some CSS spacing issues

--- src/views/LandingPage/Landing.js
+++ src/views/LandingPage/Landing.js
@@ -40,6 +40,7 @@ export default function Landing() {
         }}
       />
       <br></br>
+
       <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
         Plan your trip
       </button>
