118ed0b7ead62d8092f64dcf52baee4937ca36f4
Brett Seifried <seifriedbrett@gmail.com>
2022-04-14 14:29:40 -0700

    fixed CSS issue

--- src/services/city.js
+++ src/services/city.js
@@ -1,10 +1,12 @@
-import { client } from './client';
+const client = `${process.env.HEORKU_URL}/api/v1/trips`;
 
-export async function addCity(city) {
+export async function addCity(location) {
   const resp = await client.from('trips').insert({
     location,
     start_date,
     end_date,
     trips_id: client.auth.users_id().id,
   });
+
+  return resp;
 }
