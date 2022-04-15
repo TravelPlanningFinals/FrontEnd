bcb54babed199fb633d3b0d29b954a56fefe1b7d
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-04-15 15:31:54 -0700

    first part into styling login page and cleaning code

--- src/views/Auth/Login.js
+++ src/views/Auth/Login.js
@@ -1,31 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import AuthLogin from '../../components/Auth/AuthLogin';
+import './Login.css';
 
 export default function Login() {
   return (
-    <div>
+    <div className="login">
       <AuthLogin />
-      <p>this page is Login.js in views/auth</p>
-
-      {/* <button class="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
-        <Link to="/register">Sign Up</Link>
-      </button>
-      <br></br> */}
-
-      {/*this button above can be deleted, seeing as we are only logging in with GITHUb, and will not need a sign up function */}
-
-      <button
-        /*onClick={app.use('/api/v1/github', require('./controllers/github'))}*/
-        class="bg-transparent hover:bg-pink-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded"
-      >
+      <h2>Sign in with Github OAuth</h2>
+      <button class="bg-transparent hover:bg-pink-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-#a2e3c4-500 hover:border-transparent rounded">
         {/* <a href={'https://travelapp70.herokuapp.com/api/v1/github/login'}> */}
         {/* !!!!!this link above auto deploys MAIN branch. We need to activate this one, and disable the one below for presentation !!!!!*/}
 
         <a href={'http://localhost:7890/api/v1/github/login'}>
           {' '}
           {/* !!!! this is dev link!!!!! */}
-          Sign in with Github
+          Sign in
         </a>
       </button>
     </div>
