4a0331b2a8e9d5e7fe4b7858c75375aa794c76a9
Libbi Dunham <Libbi.Dunham25@gmail.com>
2022-04-15 12:52:20 -0700

    fixed links and added a signout function

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { signOut } from '../../services/users';
 
 export default function Header() {
   return (
     <div>
       <Link to="/">Back to Home (this is header)</Link>
-      <button onClick={logout}>Logout</button>
+      <button onClick={signOut}>Logout</button>
     </div>
   );
 }
