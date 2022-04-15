1dda4833d9f3d508bcb3ef2fdadfad18f802e4da
Brett Seifried <seifriedbrett@gmail.com>
2022-04-15 11:10:16 -0700

    AuthContext.jsx completed

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,14 +1,35 @@
-import { createContext, useEffect, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
+import fetchUser from '../services/user';
 
+const UserContext = createContext();
 
-export const authContext = createContext();
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({});
 
-export function ProvideAuth({ children }) {
-    const [user, setUser] = useState(null);
+  useEffect(() => {
+    fetchUser()
+      .then((fetchedUser) => {
+        setUser(fetchedUser);
+      })
+      .catch((error) => {
+        throw new Error(`Error: ${error}`);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetchUs
-    })
-    }
+  const contextValue = { user };
 
-}
+  return (
+    <UserContext.Provider value={contextValue}>{children}</UserContext.Provider>
+  );
+};
+
+const useUser = () => {
+  const context = useContext(UserContext);
+
+  if (context === undefined) {
+    throw new Error('useUser must be used within a UserContext Provider');
+  }
+  return context;
+};
+
+export { UserProvider, useUser };
