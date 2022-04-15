e56b51634ea19c47d9ea58ec0b44cb7be1f68436
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-04-15 15:35:11 -0700

    deleted auth component because it seemed like an extra step. Login page wont have a lot on it, so best to keep it just in the view itself

--- src/views/Auth/Login.js
+++ src/views/Auth/Login.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import AuthLogin from '../../components/Auth/AuthLogin';
 import './Login.css';
 
 export default function Login() {
   return (
     <div className="login">
-      <AuthLogin />
       <h2>Sign in with Github OAuth</h2>
       <button class="bg-transparent hover:bg-pink-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-#a2e3c4-500 hover:border-transparent rounded">
         {/* <a href={'https://travelapp70.herokuapp.com/api/v1/github/login'}> */}
