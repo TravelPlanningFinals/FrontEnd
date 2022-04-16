73d56766c0bef410bbaed5fa34838e326821cede
Brett Seifried <seifriedbrett@gmail.com>
2022-04-15 18:13:41 -0700

    fixing merge

--- src/views/Auth/Login.js
+++ src/views/Auth/Login.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import './Login.css';
+import { Link } from 'react-router-dom';
+import AuthLogin from '../../components/Auth/AuthLogin';
 
 export default function Login() {
   return (
@@ -15,16 +16,13 @@ export default function Login() {
         /*onClick={app.use('/api/v1/github', require('./controllers/github'))}*/
         class="bg-transparent hover:bg-pink-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-red-500 hover:border-transparent rounded"
       >
-    <div className="login">
-      <h2 className="signin-message">Sign in with Github OAuth</h2>
-      <button class="bg-transparent hover:bg-pink-500 text-red-700 font-semibold hover:text-white py-2 px-4 border border-#a2e3c4-500 hover:border-transparent rounded m-6">
         {/* <a href={'https://travelapp70.herokuapp.com/api/v1/github/login'}> */}
         {/* !!!!!this link above auto deploys MAIN branch. We need to activate this one, and disable the one below for presentation !!!!!*/}
 
         <a href={'http://localhost:7890/api/v1/github/login'}>
           {' '}
           {/* !!!! this is dev link!!!!! */}
-          Sign in
+          Sign in with Github
         </a>
       </button>
     </div>
