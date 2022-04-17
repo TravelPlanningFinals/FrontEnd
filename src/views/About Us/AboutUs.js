2b78438caec3081608d8060ccb01f476b85096c1
Libbi Dunham(she/her) <89104000+Libbi-Dunham@users.noreply.github.com>
2022-04-17 11:56:46 -0700

    Libbi (#23)
    * made a form for tripDetails
    
    * uncommented out trips import
    
    * added some css to tripdetails
    
    * updated about us page

--- src/views/About Us/AboutUs.js
+++ src/views/About Us/AboutUs.js
@@ -1,71 +1,78 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import './AboutUs.css';
-import linkedin from '../../assets/AboutUs/linkedin.png';
-import libbi_linkedin from '../../assets/AboutUs/PFP/libbi_linkedin.jpeg';
-import noah_linkedin from '../../assets/AboutUs/PFP/noah_linkedin.jpeg';
+import libbi from '../../assets/AboutUs/PFP/libbi.jpeg';
+import noah from '../../assets/AboutUs/PFP/noah.jpeg';
 import brett from '../../assets/AboutUs/PFP/brett.png';
+import chase from '../../assets/AboutUs/PFP/chase.jpg';
+import github from '../../assets/AboutUs/PFP/github.png';
 
 export default function AboutUs() {
   return (
-    <body>
-      <div className="aboutus">
-        <br></br>
-        <h1>About the Creators</h1>
-        <br></br>
-        <Link
-          to={{ pathname: 'https://www.linkedin.com/in/libbi-dunham/' }}
-          target="_blank"
-        >
-          <div className="us">
-            <img src={libbi_linkedin} className="pfp"></img>
-            <img src={linkedin} className="linkedin"></img>
-          </div>
-          <h2>Libbi</h2>
-        </Link>
-        <p>
-          insert bio here, drop the linked in links in the chat when we preset
-        </p>
-        <Link
-          to={{ pathname: 'https://www.linkedin.com/in/brett-seifried/' }}
-          target="_blank"
-        >
-          <div className="us">
-            <img src={brett} className="pfp"></img>
-            <img src={linkedin} className="linkedin"></img>
-          </div>
-          <h2>Brett</h2>
-        </Link>
-        <p>
-          insert bio here, drop the linked in links in the chat when we preset
-        </p>
-        <Link
-          to={{ pathname: 'https://www.linkedin.com/in/noahdeltorosmyth/' }}
-          target="_blank"
-        >
-          <div className="us">
-            <img src={noah_linkedin} className="pfp"></img>
-            <img src={linkedin} className="linkedin"></img>
-          </div>
-          <h2>Noah</h2>
-        </Link>
-        <p>
-          insert bio here, drop the linked in links in the chat when we preset
-        </p>
-        <Link
-          to={{ pathname: 'https://www.linkedin.com/in/gregory-crowder/' }}
-          target="_blank"
-        >
-          <div className="us">
-            {/* <img src={} className="pfp"></img> */}
-            <img src={linkedin} className="linkedin"></img>
-          </div>
-          <h2>Chase</h2>
-        </Link>
-        <p>
-          insert bio here, drop the linked in links in the chat when we preset
-        </p>
+    <div className="about-container">
+      <h1>About Us</h1>
+      <div className="about-container_desc">
+        <p>Meet the creators of Travel App.....!</p>
       </div>
-    </body>
+      <div className="bio-container">
+        <div className="bio-container_card">
+          <h4>
+            <a href="https://www.linkedin.com/in/libbi-dunham/" target="_blank">
+              Libbi Dunham
+            </a>
+          </h4>
+          <a href="https://github.com/Libbi-Dunham" target="_blank">
+            <img className="github" src={github} alt="libbi github" />
+          </a>
+          <img className="bio-img" src={libbi} alt="libbi headshot" />
+
+          <p>Libbi is a Software developer.</p>
+        </div>
+        <div className="bio-container_card">
+          <h4>
+            <a
+              href="https://www.linkedin.com/in/brett-seifried/"
+              target="_blank"
+            >
+              Brett seifried
+            </a>
+          </h4>
+          <a href="https://github.com/BrettSeifried" target="_blank">
+            <img className="github" src={github} alt="brett github" />
+          </a>
+          <img className="bio-img" src={brett} alt="brett headshot" />
+          <p>Brett is a web developer.</p>
+        </div>
+        <div className="bio-container_card">
+          <h4>
+            <a
+              href="https://www.linkedin.com/in/noahdeltorosmyth/"
+              target="_blank"
+            >
+              Noah Smyth
+            </a>
+          </h4>
+          <a href="https://github.com/NoahDeltoroSmyth" target="_blank">
+            <img className="github" src={github} alt="noah github" />
+          </a>
+          <img className="bio-img" src={noah} alt="noah headshot" />
+          <p>Noah is a web developer.</p>
+        </div>
+        <div className="bio-container_card">
+          <h4>
+            <a
+              href="https://www.linkedin.com/in/gregory-crowder/"
+              target="_blank"
+            >
+              Chase Crowder
+            </a>
+          </h4>
+          <a href="https://github.com/Gcrowder93" target="_blank">
+            <img className="github" src={github} alt="chase github" />
+          </a>
+          <img className="bio-img" src={chase} alt="chase headshot" />
+          <p>Chase is a software developer.</p>
+        </div>
+      </div>
+    </div>
   );
 }
