3edb363da200e648d02df7e2d90f15eec9d7fc4a
Chase Crowder <gcrowder93@gmail.com>
2022-04-18 13:01:28 -0700

    mapping through trip card on trip page works (#32)

--- src/views/Trips/Trips.js
+++ src/views/Trips/Trips.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import TripDetails from '../../components/TripDetails/TripDetails';
+<<<<<<< HEAD
+import TripsCard from './TripsCard';
+=======
 
 import TripsCard from './TripsCard';
 
+>>>>>>> b13d8190c1b54a227935501ad1eec397925b4693
 import { getAllTrips } from '../../services/params';
 
 export default function Trips() {
