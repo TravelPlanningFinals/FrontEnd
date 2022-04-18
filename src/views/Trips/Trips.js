c297fe567c2983ef5282dd7768c97e19e235d40f
Brett Seifried <seifriedbrett@gmail.com>
2022-04-18 12:48:47 -0700

    Brett (#29)
    * tried setting up user in city of trip. added what to display and CSS
    
    * added image and CSS to travel destination
    
    * context example on tripslocationdetails.js. dates set up, group page set up, flight status started
    
    * updated plan page to 6 tiles, removed chat and budget
    
    * skeleton for ldogin created
    
    * entertianment card set up in entertianment.js
    
    * started work on /:id

--- src/views/Trips/Trips.js
+++ src/views/Trips/Trips.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import TripDetails from '../../components/TripDetails/TripDetails';
+import { getAllTrips } from '../../services/params';
 
 export default function Trips() {
+  const tripList = getAllTrips();
+  console.log('getAllTrips', getAllTrips([]));
+  console.log('tripList', tripList);
+
   return (
     <>
       <TripDetails />
