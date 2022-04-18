f452e98d7fae027c71d7792c557ad3c9aeb91ffa
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-04-18 11:39:00 -0700

    can successfully add 4 flights information with 1 button click

--- src/services/flights.js
+++ src/services/flights.js
@@ -1,11 +1,16 @@
-export async function addFlights(airline, departure, arrival) {
+export async function addFlights(airline, departure, arrival, flightNumber) {
   try {
     const res = await fetch(`${process.env.HEROKU_URL}/api/v1/flights`, {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       credentials: 'include',
       mode: 'cors',
-      body: JSON.stringify({ airline, departure, arrival }),
+      body: JSON.stringify({
+        airline,
+        departure,
+        arrival,
+        flightNumber,
+      }),
     });
     console.log('res', res);
     return res.json();
