eaccc3a255481d06f9345c98889f158b2e1cf7b8
Brett Seifried <seifriedbrett@gmail.com>
2022-04-17 20:30:53 -0700

    added image and CSS to travel destination

--- src/views/Plan/TripLocation.js
+++ src/views/Plan/TripLocation.js
@@ -2,36 +2,36 @@ import React, { useState } from 'react';
 import TripLocationDetails from '../../components/Plan/TripLocation/TripLocationDetails';
 import X from '../../assets/images/X.png';
 import { useUser } from '../../context/UserProvider';
+import travel from '../../assets/images/travel.png';
 
 export default function TripLocation() {
   const [style, setStyle] = useState('tripInfo');
   const [hidden, setHidden] = useState('hidden');
   const [XStyle, setXStyle] = useState('hidden');
+  const [image, setImage] = useState('travel');
+  const { user } = useUser();
 
   function handleClick() {
     setStyle('tripInfoBig');
     setHidden(null);
     setXStyle('X');
+    setImage('hidden');
   }
 
   function minimize() {
     setStyle('tripInfo');
     setHidden('hidden');
     setXStyle('hidden');
+    setImage('travel');
   }
 
   return (
     <div>
       <img onClick={minimize} src={X} class={XStyle}></img>
-      <div
-        // onClick={ToggleClass}
-        onClick={handleClick}
-        className={style}
-        // className={tripInfo ? 'tripInfo' : 'tripInfoBig'}
-      >
+      <div onClick={handleClick} className={style}>
         {/* pull from back end */}
         <h1 className="padding">CITY going to</h1>
-        {/* <div className={hidden ? 'hidden' : 'tripInfoShow'}> */}
+        <img src={travel} class={image}></img>
         <div className={hidden}>
           <TripLocationDetails />
         </div>
