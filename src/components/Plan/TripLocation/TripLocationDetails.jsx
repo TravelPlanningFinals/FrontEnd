a1dcc503473fb936eb065826c4cab3bea63b596a
Brett Seifried <seifriedbrett@gmail.com>
2022-04-17 22:21:26 -0700

    Brett (#24)
    * tried setting up user in city of trip. added what to display and CSS
    
    * added image and CSS to travel destination
    
    * context example on tripslocationdetails.js. dates set up, group page set up, flight status started

--- src/components/Plan/TripLocation/TripLocationDetails.jsx
+++ src/components/Plan/TripLocation/TripLocationDetails.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import { useUser } from '../../../context/UserProvider';
 import './tripLocation.css';
-import X from '../../../assets/images/X.png';
 
 export default function TripLocationDetails() {
+  // --- Need to use this context below ----
+  const { user } = useUser();
+
+  console.log('user', user);
+
   return (
-    <div>
+    <div class="tripCard">
       <div>Trip Location Page</div>
       <h1>Your Trip</h1>
+      <div>
+        <ul>
+          {/* --- user context here for user names "group" --- */}
+          <li>User Context's Group</li>
+          {/* user context example here */}
+          <li>{user?.username}</li>
+          {/* --- use the TripId to pull dates going */}
+          <li> Date of whent they are going</li>
+          {/* --- back end of tripId to show where they are going */}
+          <li>Where they are going</li>
+          {/* --- Start Date count down of when to go. */}
+          <li>count down until event</li>
+        </ul>
+      </div>
       <input type="text" placeholder="testing"></input>
     </div>
   );
--- src/views/Plan/Dates.js
+++ src/views/Plan/Dates.js
@@ -1,22 +1,26 @@
 import React, { useState } from 'react';
 import DateDetails from '../../components/Plan/Dates/DateDetails';
 import X from '../../assets/images/X.png';
+import calendar from '../../assets/images/calendar.png';
 
 export default function Dates() {
   const [style, setStyle] = useState('dateInfo');
   const [hidden, setHidden] = useState('hidden');
   const [XStyle, setXStyle] = useState('hidden');
+  const [image, setImage] = useState('calendar');
 
   function handleClick() {
     setStyle('dateInfoBig');
     setHidden(null);
     setXStyle('X');
+    setImage('hidden');
   }
 
   function minimize() {
     setStyle('dateInfo');
     setHidden('hidden');
     setXStyle('hidden');
+    setImage('calendar');
   }
 
   return (
@@ -24,7 +28,11 @@ export default function Dates() {
       <img onClick={minimize} src={X} class={XStyle}></img>
 
       <div onClick={handleClick} className={style}>
+        {/* --- Show Start and end Date of Trip from back end --- */}
         <h1 className="padding">Date Page</h1>
+        {/* --- Count down to start date --- */}
+        <h2>Count down until Trip</h2>
+        <img src={calendar} class={image}></img>
         <div className={hidden}>
           <DateDetails />
         </div>
