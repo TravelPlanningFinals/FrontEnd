457c845add5221a809d09c405e1ade51055c4c8b
Brett Seifried <seifriedbrett@gmail.com>
2022-04-18 10:11:15 -0700

    entertianment card set up in entertianment.js

--- src/views/Plan/Entertainment.js
+++ src/views/Plan/Entertainment.js
@@ -1,22 +1,26 @@
 import React, { useState } from 'react';
 import EntertainmentDetails from '../../components/Plan/Entertainment/EntertainmentDetails';
 import X from '../../assets/images/X.png';
+import entertainment from '../../assets/images/entertainment.png';
 
 export default function Entertainment() {
   const [style, setStyle] = useState('entertainmentInfo');
   const [hidden, setHidden] = useState('hidden');
   const [XStyle, setXStyle] = useState('hidden');
+  const [image, setImage] = useState('entertainmentStyle');
 
   function handleClick() {
     setStyle('entertainmentInfoBig');
     setHidden(null);
     setXStyle('X');
+    setImage('hidden');
   }
 
   function minimize() {
     setStyle('entertainmentInfo');
     setHidden('hidden');
     setXStyle('hidden');
+    setImage('entertainmentStyle');
   }
 
   return (
@@ -24,6 +28,7 @@ export default function Entertainment() {
       <img onClick={minimize} src={X} class={XStyle}></img>
       <div onClick={handleClick} className={style}>
         <h1 className="padding">Entertainment Page</h1>
+        <img src={entertainment} class={image}></img>
         <div className={hidden}>
           <EntertainmentDetails />
         </div>
