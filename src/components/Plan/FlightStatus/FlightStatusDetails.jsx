826a1cc5d4bc7221c6add5198e412fa263ee8234
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2022-04-18 10:36:19 -0700

    user can successfully add flight information into backend table. created addFlights fetch call

--- src/components/Plan/FlightStatus/FlightStatusDetails.jsx
+++ src/components/Plan/FlightStatus/FlightStatusDetails.jsx
@@ -1,41 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { addFlights } from '../../../services/flights';
 import './flightStatus.css';
 
 export default function FlightStatusDetails() {
+  const [name, setName] = useState('');
+  const [airline, setAirline] = useState('');
+  const [departure, setDeparture] = useState('');
+  const [arrival, setArrival] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await addFlights(airline, departure, arrival);
+  };
+
   return (
     <form class="flightInputs">
       <div className="first">
         {/* --- 1st Person flight info --- */}
-        <input placeholder="Name" />
-        <input placeholder="Flight Airline" />
-        <input placeholder="Departure" />
-        <input placeholder="Arrival" />
-        <input placeholder="Flight Number" />
+        {/* <input
+          placeholder="Name"
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        /> */}
+        <input
+          placeholder="Flight Airline"
+          value={airline}
+          onChange={(e) => {
+            setAirline(e.target.value);
+          }}
+        />
+        <input
+          placeholder="Departure"
+          value={departure}
+          onChange={(e) => {
+            setDeparture(e.target.value);
+          }}
+        />
+        <input
+          placeholder="Arrival"
+          value={arrival}
+          onChange={(e) => {
+            setArrival(e.target.value);
+          }}
+        />
+        {/* <input placeholder="Flight Number" /> */}
       </div>
-      <div className="second">
-        {/* Second person flight info */}
-        <input placeholder="Name" />
-        <input placeholder="Flight Airline" />
-        <input placeholder="Departure" />
-        <input placeholder="Arrival" />
-        <input placeholder="Flight Number" />
-      </div>
-      <div className="third">
-        {/* Second person flight info */}
-        <input placeholder="Name" />
-        <input placeholder="Flight Airline" />
-        <input placeholder="Departure" />
-        <input placeholder="Arrival" />
-        <input placeholder="Flight Number" />
-      </div>
-      <div className="fourth">
-        {/* Second person flight info */}
-        <input placeholder="Name" />
-        <input placeholder="Flight Airline" />
-        <input placeholder="Departure" />
-        <input placeholder="Arrival" />
-        <input placeholder="Flight Number" />
-      </div>
+      <button onClick={handleSubmit}>Add new flight information</button>
     </form>
   );
 }
