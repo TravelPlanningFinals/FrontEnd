feed3577d59962393a6b93eda4eebbc044b06e9c
Brett Seifried <seifriedbrett@gmail.com>
2022-04-18 22:08:16 -0700

    groupForm fix

--- src/components/Plan/Group/GroupDetails.jsx
+++ src/components/Plan/Group/GroupDetails.jsx
@@ -4,13 +4,6 @@ import './group.css';
 import GroupForm from './GroupForm';
 
 export default function GroupDetails() {
-  // --- Add user context ---
-  // const { user } = useUser();
-  // const flight from back end
-
-  // Create and Edit button
-  // Convert inputs to text
-  // Check notes on Kanban for how to add users (gabe)
   return (
     <div>
       <GroupForm />
