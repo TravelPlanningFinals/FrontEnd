8703f68e7c4d5072e4f1ba57ad6baba833294e74
Chase Crowder <gcrowder93@gmail.com>
2022-04-19 08:24:42 -0700

    Chasedev (#44)
    * mapping through trip card on trip page works
    
    * Trips card import error
    
    * button now reloads trips
    
    * styled trip list

--- src/views/Trips/TripsCard.js
+++ src/views/Trips/TripsCard.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useTrips } from '../../context/TripProvider';
 import { getAllTrips } from '../../services/trips';
+import './TripsCard.css';
 
 export default function TripsCard() {
   const { trips, setTrips } = useTrips();
@@ -16,22 +17,23 @@ export default function TripsCard() {
   }, []);
   console.log(trips);
   return (
-    <div>
+    <>
       <h1 className="tripListTitle">TRIPS</h1>
-
-      {trips.map((trips) => {
-        return (
-          <div className="tripsList" key={trips.id}>
-            <Link className="tripLink" to={`/plan/${trips.id}`}>
-              <mark className="tripLocation">
-                Destination: {trips.location}
-              </mark>
-            </Link>
-            <p className="tripStartDate">Start Date: {trips.startDate}</p>
-            <p className="tripEndDate">End Date: {trips.endDate}</p>
-          </div>
-        );
-      })}
-    </div>
+      <div className="trips">
+        {trips.map((trips) => {
+          return (
+            <div className="tripsList" key={trips.id}>
+              <Link className="tripLink" to={`/plan/${trips.id}`}>
+                <a className="tripLocation">Destination: {trips.location}</a>
+              </Link>
+              <div className="tripdates">
+                <p className="tripStartDate">Start Date: {trips.startDate}</p>
+                <p className="tripEndDate">End Date: {trips.endDate}</p>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
