552f5011d59bcda8cd335cb2dee91dd486506e7d
Brett Seifried <seifriedbrett@gmail.com>
2022-04-19 12:24:21 -0700

    started work on entertainment page

--- src/components/Plan/Entertainment/EntertainmentDetails.jsx
+++ src/components/Plan/Entertainment/EntertainmentDetails.jsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { getTripsById } from '../../../services/trips';
+import { getYelp } from '../../../utils/yelp';
 import './entertainment.css';
 
 export default function EntertainmentDetails() {
-  return <div>EntertainmentDetails</div>;
+  const location = 'Reno';
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const yelp = await getYelp(location);
+      console.log('yelp', yelp);
+    };
+    fetchData();
+  }, []);
+
+  return <h1> Test</h1>;
 }
