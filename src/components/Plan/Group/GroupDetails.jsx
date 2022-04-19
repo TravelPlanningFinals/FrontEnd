80d82f9c72ab709406c382d1dc2ba7bc4a9a1bc8
Brett Seifried <seifriedbrett@gmail.com>
2022-04-18 22:15:33 -0700

    fixing merge issue

--- src/components/Plan/Group/GroupDetails.jsx
+++ src/components/Plan/Group/GroupDetails.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useUser } from '../../../context/UserProvider';
 import './group.css';
-import GroupAddForm from './GroupAddForm';
+import GroupAddForm from './GroupAddFrom';
 
 export default function GroupDetails() {
   return (
