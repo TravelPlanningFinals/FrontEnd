11fd9d03522ac33eecd6384bd112ccb61658a3fe
Brett Seifried <seifriedbrett@gmail.com>
2022-04-19 17:13:40 -0700

    refactored calendar page

--- src/views/Plan/Dates.js
+++ src/views/Plan/Dates.js
@@ -5,55 +5,20 @@ import calendar from '../../assets/images/calendar.png';
 import { useParams } from 'react-router-dom';
 import { getTripsById } from '../../services/trips';
 import CountDown from '../../utils/countdown';
+import { useTrips } from '../../hooks/useTrips';
 
 export default function Dates() {
-  const { id } = useParams();
-  const [startDate, setStartDate] = useState('');
+  const { trips } = useTrips();
+  const count = CountDown(trips.startDate);
 
-  const [style, setStyle] = useState('dateInfo');
-  const [hidden, setHidden] = useState('hidden');
-  const [XStyle, setXStyle] = useState('hidden');
-  const [image, setImage] = useState('calendar');
-
-  function handleClick() {
-    setStyle('dateInfoBig');
-    setHidden(null);
-    setXStyle('X');
-    setImage('hidden');
-  }
-
-  function minimize() {
-    setStyle('dateInfo');
-    setHidden('hidden');
-    setXStyle('hidden');
-    setImage('calendar');
-  }
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const data = await getTripsById(id);
-      const day = data.startDate;
-      setStartDate(day);
-    };
-    fetchData();
-  }, []);
-
-  const count = CountDown(startDate);
-  console.log('count', count);
+  console.log('tripsClaendar', trips.startDate);
 
   return (
     <div>
-      <img onClick={minimize} src={X} class={XStyle}></img>
-
-      <div onClick={handleClick} className={style}>
-        {/* --- Show Start and end Date of Trip from back end --- */}
-        <h1 className="padding">Date Page</h1>
-        {/* --- Count down to start date --- */}
+      <div className="dateInfo">
         <h2>Count down until Trip</h2>
-        <img src={calendar} class={image}></img>
-        <div className={hidden}>
-          <DateDetails />
-        </div>
+        <h1>{count} Days</h1>
+        <img src={calendar} class="calendar"></img>
       </div>
     </div>
   );
