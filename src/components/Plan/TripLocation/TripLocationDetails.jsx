c184c8f9807d0e63b8f2ab337d0eb038ec046193
Brett Seifried <seifriedbrett@gmail.com>
2022-04-20 09:58:35 -0700

    fixed protected Route

--- src/components/Plan/TripLocation/TripLocationDetails.jsx
+++ src/components/Plan/TripLocation/TripLocationDetails.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import { useUser } from '../../../context/UserProvider';
 import { useTrips } from '../../../hooks/useTrips';
+import CountDown from '../../../utils/countdown';
 import './tripLocation.css';
 
 export default function TripLocationDetails() {
   const { user } = useUser();
   const { trips, loading } = useTrips();
-  console.log('Look here NOAH', trips.guests);
+  const count = CountDown(trips.startDate);
+  console.log('trips', trips);
 
   if (loading) return <p>...loading</p>;
 
   return (
     <div class="tripCard">
       <div>
-        <ul>
-          <li>Trip Summary for {trips.location}</li>
+        <ul class="tripSummary">
           <li>{user?.username} Will be your Tour guide</li>
           <li>
             <img src={user?.avatar} />
@@ -26,17 +27,15 @@ export default function TripLocationDetails() {
             {trips.guests.map((guest) => {
               return (
                 <div key={guest.id}>
-                  <h2>{guest.name}</h2>
-                  <h2>{guest.phone_number}</h2>
+                  <h3>{guest.name}</h3>
+                  <h3>{guest.phone_number}</h3>
                 </div>
               );
             })}
           </li>
-          {/* --- Start Date count down of when to go. */}
-          <li>count down until event</li>
+          <li>Days Until Trip: {count}</li>
         </ul>
       </div>
-      <input type="text" placeholder="testing"></input>
     </div>
   );
 }
