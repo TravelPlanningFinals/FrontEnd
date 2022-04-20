cabb1650b51612950868471ed712aea84d423cf8
Brett Seifried <seifriedbrett@gmail.com>
2022-04-20 11:03:24 -0700

    adding in / on login.js

--- src/views/Auth/Login.js
+++ src/views/Auth/Login.js
@@ -12,7 +12,7 @@ export default function Login() {
         {/* <a href={'https://travelapp70.herokuapp.com/api/v1/github/login'}> */}
         {/* !!!!!this link above auto deploys MAIN branch. We need to activate this one, and disable the one below for presentation !!!!!*/}
 
-        <a href={`${process.env.HEROKU_URL}api/v1/github/login`}>
+        <a href={`${process.env.HEROKU_URL}/api/v1/github/login`}>
           {' '}
           {/* !!!! this is dev link!!!!! */}
           Sign in
