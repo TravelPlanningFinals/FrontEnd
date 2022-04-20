5c5fe3f8d463ac73739d5ece30a262de2976a50e
Brett Seifried <seifriedbrett@gmail.com>
2022-04-20 14:53:04 -0700

    Passing about us link working and renders

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,25 +1,12 @@
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import { BrowserRouter } from 'react-router-dom';
 import App from '../src/App';
-import Header from '../src/components/Header/Header';
-import { TripProvider } from '../src/context/TripProvider';
 import { UserProvider } from '../src/context/UserProvider';
-import Login from '../src/views/Auth/Login';
+import userEvent from '@testing-library/user-event';
 
 test('Renders landing page/login loads', () => {
-  const container = render(
-    // <BrowserRouter>
-    //   <Header>
-    //     <main>
-    //       <UserProvider>
-    //         <TripProvider>
-    <App />
-    //         </TripProvider>
-    //       </UserProvider>
-    //     </main>
-    //   </Header>
-    // </BrowserRouter>
-  );
+  const container = render(<App />);
 
   const signInButton = screen.getByRole('button', { name: /sign in/i });
   const aboutUsLink = screen.getByRole('link', { name: /about us/i });
@@ -31,4 +18,32 @@ test('Renders landing page/login loads', () => {
   expect(signOut).toBeInTheDocument();
   expect(tripsLink).toBeInTheDocument();
   expect(container).toMatchSnapshot();
+});
+
+test('About us Renders', () => {
+  const container = render(
+    <BrowserRouter>
+      <UserProvider>
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      </UserProvider>
+    </BrowserRouter>
+  );
+
+  const aboutUsLink = screen.getByRole('link', { name: /about us/i });
+
+  userEvent.click(aboutUsLink);
+
+  const aboutUsHeader = screen.getByText(
+    /meet the creators of travel app\.\.\.\.\.!/i
+  );
+  const img = screen.getByRole('img', { name: /libbi headshot/i });
+  const gitHub = screen.getByRole('img', { name: /libbi headshot/i });
+  const descrip = screen.getByRole('img', { name: /libbi headshot/i });
+
+  expect(aboutUsHeader).toBeInTheDocument();
+  expect(img).toBeInTheDocument();
+  expect(gitHub).toBeInTheDocument();
+  expect(descrip).toBeInTheDocument();
 });
