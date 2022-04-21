f041215c1a5c45d0c8bbd5ad1c95f988d47ac609
Brett Seifried <seifriedbrett@gmail.com>
2022-04-20 20:21:46 -0700

    merge issue

--- src/services/lodging.js
+++ src/services/lodging.js
@@ -31,44 +31,8 @@ export async function addLodging(
         tripsId,
       }),
     });
-<<<<<<< HEAD
-    console.log('lodging res', res);
-=======
->>>>>>> 575014453726a33d286d3fce7701e8dc12a3bd6c
     return res.json();
   } catch (error) {
     console.log('there was an error', error);
   }
-<<<<<<< HEAD
 }
-
-// export async function updateLodging(
-//   id,
-//   nameOfPlace,
-//   contactInfo,
-//   pricePerNight,
-//   checkIn,
-//   checkOut,
-//   address1,
-//   address2,
-//   city,
-//   state,
-//   zip
-// ) {
-//   let resp = client.from('lodging').update({
-//     nameOfPlace,
-//     contactInfo,
-//     pricePerNight,
-//     checkIn,
-//     checkOut,
-//     address1,
-//     address2,
-//     city,
-//     state,
-//     zip,
-//   });
-//   return checkError(response);
-// }
-=======
-}
->>>>>>> 575014453726a33d286d3fce7701e8dc12a3bd6c
