1cc9925a4e955d938676d1484a666c6c96f33611
Brett Seifried <seifriedbrett@gmail.com>
2022-04-20 17:51:23 -0700

    lodging posting to the back end

--- src/services/lodging.js
+++ src/services/lodging.js
@@ -1,12 +1,11 @@
-const client = `${process.env.HEROKU_URL}/api/v1/lodging`;
+// const client = `${process.env.HEROKU_URL}/api/v1/lodging`;
 
-export async function getLodgingById(id) {
-  let resp = await client.from('lodging').select().match({ id }).single();
-  return checkError(resposne);
-}
+// export async function getLodgingById(id) {
+//   let resp = await client.from('lodging').select().match({ id }).single();
+//   return checkError(resposne);
+// }
 
-export async function updateLodging(
-  id,
+export async function addLodging(
   nameOfPlace,
   contactInfo,
   pricePerNight,
@@ -16,21 +15,60 @@ export async function updateLodging(
   address2,
   city,
   state,
-  zip
+  zip,
+  tripsId
 ) {
-  let resp = client
-    .from('lodging')
-    .update({
-      nameOfPlace,
-      contactInfo,
-      pricePerNight,
-      checkIn,
-      checkOut,
-      address1,
-      address2,
-      city,
-      state,
-      zip,
+  try {
+    const res = await fetch(`${process.env.HEROKU_URL}/api/v1/lodging`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      credentials: 'include',
+      mode: 'cors',
+      body: JSON.stringify({
+        nameOfPlace,
+        contactInfo,
+        pricePerNight,
+        checkIn,
+        checkOut,
+        address1,
+        address2,
+        city,
+        state,
+        zip,
+        tripsId,
+      }),
     });
-  return checkError(response);
+    console.log('lodging res', res);
+    return res.json();
+  } catch (error) {
+    console.log('there was an error', error);
+  }
 }
+
+// export async function updateLodging(
+//   id,
+//   nameOfPlace,
+//   contactInfo,
+//   pricePerNight,
+//   checkIn,
+//   checkOut,
+//   address1,
+//   address2,
+//   city,
+//   state,
+//   zip
+// ) {
+//   let resp = client.from('lodging').update({
+//     nameOfPlace,
+//     contactInfo,
+//     pricePerNight,
+//     checkIn,
+//     checkOut,
+//     address1,
+//     address2,
+//     city,
+//     state,
+//     zip,
+//   });
+//   return checkError(response);
+// }
