38cbb6d7131abd9220a739d2b44013dbacbab267
Noah Del Toro Smyth <87677895+NoahDeltoroSmyth@users.noreply.github.com>
2022-04-21 15:34:27 -0700

    fixed css on entertainment page (#89)

--- src/components/Plan/Entertainment/EntertainmentDetails.jsx
+++ src/components/Plan/Entertainment/EntertainmentDetails.jsx
@@ -23,22 +23,19 @@ export default function EntertainmentDetails() {
         {city.map((item) => {
           return (
             <div className="yelpdiv" key={item.name}>
-              <div>
-                <a className="yelplink" href={item.url} target="_blank">
-                  <h2 key="item">{item.name}</h2>
-
-                  <img src={item.image_url} />
-                  <div className="yelpinfo">
-                    <h2>{item.display_phone}</h2>
-                    <h2>
-                      {item.location.address1} {item.location.city}{' '}
-                      {item.location.zip_code}
-                    </h2>
-                    <h2>Price:{item.price}</h2>
-                    <h2>Rating:{item.rating}</h2>
-                  </div>
-                </a>
-              </div>
+              <a className="yelplink" href={item.url} target="_blank">
+                <h2 key="item">{item.name}</h2>
+                <img className="img" src={item.image_url} />
+                <div className="yelpinfo">
+                  <h2>{item.display_phone}</h2>
+                  <h2>
+                    {item.location.address1} {item.location.city}{' '}
+                    {item.location.zip_code}
+                  </h2>
+                  <h2>Price:{item.price}</h2>
+                  <h2>Rating:{item.rating}</h2>
+                </div>
+              </a>
             </div>
           );
         })}
