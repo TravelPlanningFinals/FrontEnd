575014453726a33d286d3fce7701e8dc12a3bd6c
Chase Crowder <gcrowder93@gmail.com>
2022-04-20 20:15:47 -0700

    adding group members, flights, etc refreshes page and shows added ite… (#66)
    * adding group members, flights, etc refreshes page and shows added items. Changed some color backgrounds in forms.
    
    * deleted easy edit calls

--- src/components/PrivateRoute/PrivateRoute.jsx
+++ src/components/PrivateRoute/PrivateRoute.jsx
@@ -3,11 +3,29 @@ import { Route } from 'react-router-dom';
 import { useUser } from '../../context/UserProvider';
 
 export default function PrivateRoute({ children, ...routeProps }) {
-  const { user, loading } = useUser();
-
+  const { userInput } = useUser();
   return (
-    <Route {...routeProps}>
-      {loading && !user ? <Redirect to="/" /> : children}
-    </Route>
+    <>
+      <Route
+        {...routeProps}
+        render={({ location }) =>
+          !userInput ? (
+            children
+          ) : (
+            <Redirect to={{ pathname: '/', state: { from: location } }} />
+          )
+        }
+      />
+    </>
   );
 }
+
+// export default function PrivateRoute({ children, ...routeProps }) {
+//   const { user, loading } = useUser();
+
+//   return (
+//     <Route {...routeProps}>
+//       {loading && !user ? <Redirect to="/" /> : children}
+//     </Route>
+//   );
+// }
